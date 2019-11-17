641c4cc3fd8e65d0beb7cf2c11e9219ccc2427b9
pablo <pablonieto02@gmail.com>
2019-11-17 01:49:02 -0300

    a

--- broke_json_firebase_to_json/app.py
+++ broke_json_firebase_to_json/app.py
@@ -1,7 +1,10 @@
 from threading import Thread
+from multiprocessing import Process
+import functions as fu
 import pandas as pd
 import json
 
+print('lendo arquivo...')
 with open('broken_original/bruto-2019-08-16.json', 'r') as f:
     data = json.load(f)
 
@@ -9,97 +12,43 @@ df = pd.DataFrame(data)
 pd.set_option('display.max_colwidth', -1)
 pd.set_option('display.max_columns', 999)
 
-def change_char(string, pos, change):
-    return string[:pos]+change+string[pos+1:]
-
-def limpar_aspas(texto):
-    while True:
-        if texto.find('\"') == -1:
-            break
-
-        left = texto.find('"') + 1
-        right = texto[left+1:].find('"') + left + 1
-        remove = texto[left:right].find("'")
-        if remove != -1:
-            texto = change_char(texto,left+remove,'')
-            texto = change_char(texto,left-1,"'")
-            texto = change_char(texto,right-1,"'")
-        else:
-            texto = change_char(texto,left-1,'')
-            texto = change_char(texto,right-1,'')
-    return texto
-
-def limpar_url(texto):
-    while True:
-        if texto.find('<a') == -1:
-            break
-
-        left = texto.find('<a') + 1
-        right = texto[left+1:].find('</a>') + left + 1
-
-        texto = texto[:left-1] + texto[right+4:]
-    return texto
-
-def limpar_aspas_simples(texto):
-    array_remove = []
-    for left in range(1, len(texto)):
-        if texto[left] == ":":
-            if texto[left:left+3] == ": '":
-                for right in range(left+2, len(texto)):
-                    if texto[right] == "'":
-                        if texto[right:right+2] == "'," or texto[right:right+2] == "'}":
-                            if left+2 != right:
-                                if texto[left+3:right].find("'") != -1:
-                                    array_remove.append(left+3 + texto[left+3:right].find("'"))
-                            break
-    for remove in array_remove:
-        texto = change_char(texto, remove, '@')
-    array_remove = None
-    return texto
-
-def gerar_arquivo(df, num_thread):
-    count = 1
-    max = len(df)
-    arquivo = '['
-    for row in df.iterrows():
-        linha = str(row[1])
-        linha = limpar_aspas(linha)
-        linha = limpar_aspas_simples(linha)
-        linha = limpar_url(linha)
-        linha = linha.replace('\\','')
-        linha = linha.replace('usuarios    {','{').replace('False', '\'False\'').replace('True', '\'True\'')
-        linha = linha.replace('\'','"' )
-        linha = linha.replace('Name: '+row[0]+', dtype: object', '').replace('dadosHistorico', str(count))
-        linha = linha.replace('}', '}\n')
-        count += 1
-        if count >= max:
-            arquivo += linha + ']'
-            break
-        else:
-            arquivo += linha + ','
-    file = open('json_tratado_t' + str(num_thread) + '.json', 'w')
-    file.write(arquivo)
-    file.close()
-
 class Th(Thread):
-    def __init__ (self, df, num_thread):
+    def __init__ (self, df, num_thread, caminho):
         Thread.__init__(self)
         self.num_thread = num_thread
         self.df = df
+        self.caminho = caminho
+    def run(self):        
+        fu.gerar_arquivo(self.df,self.num_thread, self.caminho)
 
-    def run(self):
-        gerar_arquivo(self.df,self.num_thread)
+# Start App
+print('start App...')
+caminho = 'json_temp/json_thread'
+len_df = len(df)
+print ('lean: ' + str(len_df))
+index = 0
+index_range = 40000
+thread_number = 0
+procs = []
 
-#for thread_number in range (2):
-thread = Th(df[0:50000], 1)
-thread.start()
-thread = Th(df[50001:100000], 2)
-thread.start()
-thread = Th(df[100001:150000], 3)
-thread.start()
-thread = Th(df[150001:20000], 4)
-thread.start()
-thread = Th(df[20001:25000], 5)
-thread.start()
-thread = Th(df[25000:25001], 6)
-thread.start()
+if __name__ == '__main__':
+    while True:
+        thread_number += 1
+        print('processor ' + str(thread_number) + ' iniciado')
+        if index + index_range < len_df:
+            end = index + index_range
+            print('index de: ' + str(index) + ' até: ' + str(end))
+            proc = Process(target=fu.gerar_arquivo, args=(df[index:end], thread_number, caminho,))
+            procs.append(proc)
+            proc.start()
+        else:
+            end = len_df
+            print('index de: ' + str(index) + ' até: ' + str(end))
+            proc = Process(target=fu.gerar_arquivo, args=(df[index:end], thread_number, caminho,))
+            procs.append(proc)
+            proc.start()
+            break
+        index += index_range
+
+    for proc in procs:
+        proc.join()
---
+++ broke_json_firebase_to_json/test_json.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+path = '/home/pablo/github/python/broke_json_firebase_to_json/json_temp'
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.json' in file:
+            files.append(os.path.join(r, file))
+
+for f in files:
+    try:
+        with open(f, 'r') as f:
+            data = json.load(f)
+            print(str(f) + ' OK!')
+    except Exception as e:
+        print(str(f) + ' ERRO!')
+        print('Failed to upload to ftp: '+ str(e))
