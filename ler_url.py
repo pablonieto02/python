044302eeb15e58092a4e9ab4f022e453f1555b2a
Pablo <pablonieto02@gmail.com>
2020-01-09 13:22:50 -0300

    a

--- ler_url.py
+++ ler_url.py
@@ -1,10 +1,19 @@
-import requests 
+import requests
+import csv 
 
 url = 'https://us-central1-oi-contabilizei.cloudfunctions.net/docs/criptografia/hash'
 
-param = 'gustavo.castanho@gmail.com'
-PARAMS = {'pass':param}
+export_file = open('user_key_pre_cadastro_no_aprovado.csv','w')
+csv_file = open('pre_cadastro_no_aprovado.csv')
 
-r = requests.get(url = url, params = PARAMS) 
+csv_reader = csv.reader(csv_file, delimiter=',')
 
-print(r.text)
+line_count = 0
+for row in csv_reader:
+    line_count = line_count + 1
+    print(line_count)
+    param = row[0]
+    PARAMS = {'pass':param}
+    retorno = requests.get(url = url, params = PARAMS) 
+    export_file.write(param + ',' + retorno.text)
+    export_file.write('\n')
