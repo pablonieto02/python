aa16fd05ae98848713f319f731e12fffd7397051
pablonieto02@gmail.com <pablonieto02@gmail.com>
2020-01-27 18:54:50 -0300

    a

--- ler_url.py
+++ ler_url.py
@@ -1,19 +1,23 @@
 import requests
-import csv 
+import csv
+import time
+import hashlib
 
 url = 'https://us-central1-oi-contabilizei.cloudfunctions.net/docs/criptografia/hash'
 
-export_file = open('user_key_pre_cadastro_no_aprovado.csv','w')
-csv_file = open('pre_cadastro_no_aprovado.csv')
+csv_file = open('novembro.csv')
+export_file = open('novembro_export_p2.csv','w')
 
 csv_reader = csv.reader(csv_file, delimiter=',')
 
 line_count = 0
 for row in csv_reader:
     line_count = line_count + 1
-    print(line_count)
-    param = row[0]
-    PARAMS = {'pass':param}
-    retorno = requests.get(url = url, params = PARAMS) 
-    export_file.write(param + ',' + retorno.text)
-    export_file.write('\n')
+    if line_count >= 2469:
+        print(line_count)
+        param = row[0]
+        PARAMS = {'pass':param}
+        retorno = requests.get(url = url, params = PARAMS) 
+        export_file.write(param + ',' + retorno.text)
+        export_file.write('\n')
+        time.sleep(0.3)
